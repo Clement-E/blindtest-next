fecf82b60b8a6f6f913c761677e289322189239d
Clément <clement.evd.dev@gmail.com>
2024-11-09 22:46:38 +0100

    adds playlist ui

--- src/app/ui/Playlist/Playlist.jsx
+++ src/app/ui/Playlist/Playlist.jsx
@@ -1,12 +1,93 @@
 import "./Playlist.css";
-import { Button } from '@/components/ui/button'
+import { CirclePlay } from 'lucide-react';
 
 function Playlist() {
 
+  const mockPlaylist = [
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    },
+    {
+      song: 'song name here',
+      artist: 'artist name'
+    }
+  ]
   return (
-    <>
-      <Button className="text-myplaylist-foreground" variant="outline">Button yolo</Button>
-    </>
+    <div className="px-4 flex flex-col gap-4 justify-center h-full py-1">
+      <div className="flex flex-col overflow-y-scroll text-sm gap-1">
+        {
+          mockPlaylist.map(song => (
+            <div key={song.song} className="flex gap-2 items-center">
+              <CirclePlay className="text-myplaylist w-4"/>
+              <span>{song.song}</span>
+              <span> - </span>
+              <span>{song.artist}</span>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   );
 }
 
