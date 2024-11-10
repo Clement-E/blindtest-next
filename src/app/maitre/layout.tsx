e781fb81f41d929954c2bc64db32fde65a8f7911
Clement Eveillard <clement.evd.dev@gmail.com>
2024-11-10 19:29:11 +0100

    test deployment vercel

--- src/app/maitre/layout.tsx
+++ src/app/maitre/layout.tsx
@@ -2,7 +2,7 @@ import AddSpotify from '@/src/app/ui/AddSpotify/AddSpotify'
 import AddPlayer from '@/src/app/ui/AddPlayer/AddPlayer'
 import Rank from '@/src/app/ui/Rank/Rank'
 import Scoreboard from '@/src/app/ui/Scoreboard/Scoreboard'
-import MediaPlayer from '@/src/app/ui/MediaPlayer/MediaPlayer'
+// import MediaPlayer from '@/src/app/ui/MediaPlayer/MediaPlayer'
 import Playlist from '@/src/app/ui/Playlist/Playlist'
 import styles from "./layout.module.css";
 
@@ -13,7 +13,7 @@ export default function Layout({children}: { children: React.ReactNode }) {
         <div className={styles.addplayer}><AddPlayer /></div>
         <div className={styles.rank}><Rank /></div>
         <div className={styles.scoreboard}><Scoreboard /></div>
-        <div className={styles.mediaplayer}><MediaPlayer /></div>
+        {/* <div className={styles.mediaplayer}><MediaPlayer /></div> */}
         <div className={styles.playlist}><Playlist /></div>
     </div>
     
