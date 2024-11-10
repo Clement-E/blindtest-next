de500315873eb57f1a0767cbcf1b2ffc1b90b32f
Clement Eveillard <clement.evd.dev@gmail.com>
2024-11-10 19:06:52 +0100

    fixes bug on theme provider import

--- .history/components/theme-provider_20241109173653.tsx
+++ .history/components/theme-provider_20241109173653.tsx
@@ -2,9 +2,7 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-// import { type ThemeProviderProps } from "next-themes/dist/index"
-
-type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
+import { type ThemeProviderProps } from "next-themes/dist/index"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
